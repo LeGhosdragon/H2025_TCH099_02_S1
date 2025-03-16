9e9ca57fc2ed67ecc26cc1d87efcd77d0761462d
AlexandreG17 <alexandre.gamache.1@ens.etsmtl.ca>
2025-03-15 22:12:55 -0400

    Ajout de id et refactor id_utilisateur a nom_utilisateur

--- sql/utilisateurs.sql
+++ sql/utilisateurs.sql
@@ -1,7 +1,9 @@
 #Table des utilisateurs, devrait contenir tout les utilisateur incluant les administrateurs
 CREATE OR REPLACE TABLE Utilisateurs(
-    id_utilisateur VARCHAR(32) NOT NULL,
-    PRIMARY KEY (id_utilisateur),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+
+    nom_utilisateur VARCHAR(32) NOT NULL UNIQUE,
+    
     #Les mots de passes doivent être stocké sous forme de mot de passe haché avec Bcrypt
     #Il n'y a pas de contrainte dans le fichier sql, les contraintes sont dans la base de donnes
     mot_de_passe VARCHAR(64) NOT NULL,
@@ -11,13 +13,11 @@ CREATE OR REPLACE TABLE Utilisateurs(
 #Utilisateurs de tests
 
 # utilisateur de test 1 : Garfield/JimDavis1945 -> $2y$10$rATwoKxPSI3B2NQuGHDeue14TRMZjB0R4XoMU6DlFER8qmLaLmKyG
-INSERT INTO Utilisateurs VALUES("Garfield","$2y$10$rATwoKxPSI3B2NQuGHDeue14TRMZjB0R4XoMU6DlFER8qmLaLmKyG","ADMIN");
+INSERT INTO Utilisateurs (nom_utilisateur,mot_de_passe,type_utilisateur) VALUES("Garfield","$2y$10$rATwoKxPSI3B2NQuGHDeue14TRMZjB0R4XoMU6DlFER8qmLaLmKyG","ADMIN");
 
 # utilisateur de test 2 : adminBrian/Password1 -> $2y$10$jPM74jRwJchtHzKNXsvjd.CJ7Dw21XDh14OEqzcB3i0VRmGCCFRDK
-INSERT INTO Utilisateurs VALUES("adminBrian","$2y$10$jPM74jRwJchtHzKNXsvjd.CJ7Dw21XDh14OEqzcB3i0VRmGCCFRDK","ADMIN");
+INSERT INTO Utilisateurs (nom_utilisateur,mot_de_passe,type_utilisateur) VALUES("adminBrian","$2y$10$jPM74jRwJchtHzKNXsvjd.CJ7Dw21XDh14OEqzcB3i0VRmGCCFRDK","ADMIN");
 
 # utilisateur de test 3 : Toni300/Password2 -> $2y$10$qawye0KIxfCmO.YVz5TrAu07Eg1rgjF0zQ5BChWvt.tlkoz2UAHYy
-INSERT INTO Utilisateurs VALUES("Toni300","$2y$10$qawye0KIxfCmO.YVz5TrAu07Eg1rgjF0zQ5BChWvt.tlkoz2UAHYy","ADMIN");
+INSERT INTO Utilisateurs  (nom_utilisateur,mot_de_passe,type_utilisateur) VALUES("Toni300","$2y$10$qawye0KIxfCmO.YVz5TrAu07Eg1rgjF0zQ5BChWvt.tlkoz2UAHYy","ADMIN");
 
-# utilisateur de test 4 : Garfield/JimDavis1945 -> $2y$10$rATwoKxPSI3B2NQuGHDeue14TRMZjB0R4XoMU6DlFER8qmLaLmKyG
-#INSERT INTO usagers VALUES("Garfield","$2y$10$rATwoKxPSI3B2NQuGHDeue14TRMZjB0R4XoMU6DlFER8qmLaLmKyG","ADMIN");
