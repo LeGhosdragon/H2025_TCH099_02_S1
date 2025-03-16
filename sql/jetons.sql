4ff7cb4da7fab4a8ed12cd32f1dfba60da6cc1f2
AlexandreG17 <alexandre.gamache.1@ens.etsmtl.ca>
2025-03-15 23:24:42 -0400

    Ajout de la table scores et de la clee etrangere dans jetons

--- sql/jetons.sql
+++ sql/jetons.sql
@@ -1,7 +1,9 @@
 CREATE OR REPLACE TABLE Jetons(
     id INT PRIMARY KEY AUTO_INCREMENT,
+    #clee etranger de id dans Utilisateurs
     id_utilisateur INT NOT NULL,
     data_jeton VARCHAR(64) NOT NULL,
     #Delai dexpiration dune heure par default
-    date_expiration DATETIME NOT NULL DEFAULT DATE_ADD(NOW(),interval 1 hour)
+    date_expiration DATETIME NOT NULL DEFAULT DATE_ADD(NOW(),interval 1 hour),
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs(id)
 );
