a04c1045cbef0042c23b22c955a056ccb132886b
AlexandreG17 <alexandre.gamache.1@ens.etsmtl.ca>
2025-03-16 20:43:34 -0400

    Ajout de l'evenement sql pour faire expirer les jetons

--- sql/jetons.sql
+++ sql/jetons.sql
@@ -4,6 +4,11 @@ CREATE OR REPLACE TABLE Jetons(
     id_utilisateur INT NOT NULL,
     data_jeton VARCHAR(64) NOT NULL,
     #Delai dexpiration dune heure par default
-    date_expiration DATETIME NOT NULL DEFAULT DATE_ADD(NOW(),interval 1 hour),
+    date_expiration DATETIME NOT NULL DEFAULT DATE_ADD(NOW(),interval 1 HOUR),
     FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs(id)
 );
+ SET GLOBAL event_scheduler=ON
+
+CREATE EVENT Expirer_jetons ON SCHEDULE
+    EVERY 1 HOUR
+    DO DELETE FROM Jetons WHERE Jetons.date_expiration < now();
